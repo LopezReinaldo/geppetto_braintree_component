92a2186e8bb661603851c21603ab0db7e400b1e3
LopezReinaldo <lopezreinaldoernesto@gmail.com>
2015-09-23 22:03:35 -0430

    Getting Customer From DB

--- geppetto_braintree_component/src/com/geppetto/braintree/dao/CustomerDao.java
+++ geppetto_braintree_component/src/com/geppetto/braintree/dao/CustomerDao.java
@@ -1,21 +1,62 @@
 package com.geppetto.braintree.dao;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.geppetto.braintree.dao.support.CustomerMapper;
 import com.geppetto.braintree.domain.core.Customer;
 import com.geppetto.braintree.interfaces.dao.ICustomerDao;
 
+
 @Repository("CustomerDao")
 public class CustomerDao implements ICustomerDao{
+	
+	private Log log = LogFactory.getLog(getClass());
+	private DataSource dataSource;
+	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
 	@Value("${find_customer_by_id.sql}")
 	private String find_customer_by_id_sql;
 	
+	@Resource(name = "dataSource")
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+		this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(
+				dataSource);
+	}
+
+	public DataSource getDataSource() {
+		return dataSource;
+	}
+	
 	@Override
 	public Customer get_customer_by_id(long customer_id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		
+		MapSqlParameterSource parameters;
+		parameters = new MapSqlParameterSource();
+		parameters.addValue("customer_id", customer_id);
+		CustomerMapper the_mapper = new CustomerMapper();
+		List<Customer> dto_list = this.namedParameterJdbcTemplate.query(
+				find_customer_by_id_sql, parameters, the_mapper);
+		System.out.println("In find_all_base_by_projectid - dto_list is: "
+				+ dto_list.size());
+		if (dto_list.size() < 1) {
+			throw new Exception("no activities for project id   " + customer_id
+					+ " were found");
+
+		}
+
+		return dto_list.get(0);
+		
 	}
 
 }
--- geppetto_braintree_component/src/com/geppetto/braintree/services/CustomerService.java
+++ geppetto_braintree_component/src/com/geppetto/braintree/services/CustomerService.java
@@ -1,17 +1,31 @@
 package com.geppetto.braintree.services;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
 
+import com.geppetto.braintree.dao.CustomerDao;
 import com.geppetto.braintree.domain.core.Customer;
 import com.geppetto.braintree.interfaces.services.ICustomerService;
 
 @Service("CustomerService")
 public class CustomerService implements ICustomerService{
+		
+	private CustomerDao customer_dao;
+
+	public CustomerDao getCustomer_dao() {
+		return customer_dao;
+	}
+
+	@Resource(name="CustomerDao")
+	public void setCustomer_dao(CustomerDao customer_dao) {
+		this.customer_dao = customer_dao;
+	}
 
 	@Override
 	public Customer find_customer(long customer_id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+				
+		return this.customer_dao.get_customer_by_id(customer_id);
 	}
 
 }
